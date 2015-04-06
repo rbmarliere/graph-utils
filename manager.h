5dfca4153398ebc59d697279e1f239d971c8c500
Ricardo Marliere <rbmarliere@gmail.com>
2015-04-06 10:11:58 -0300

    estruturando modelo

--- manager.h
+++ manager.h
@@ -4,4 +4,8 @@ class Manager {
 	// classe responsável pela leitura/escrita de arquivos e montagem do grafo
 
 	void exportGraph(Graph* graph, File* file);
+
+	Graph* createGraphFromFile(File* file);
+
+	File* readFile(char* path);
 }
