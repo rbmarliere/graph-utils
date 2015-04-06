c02f7547f4071baa889c01336edb5c0829fd50d9
Ricardo Marliere <rbmarliere@gmail.com>
2015-04-06 14:43:35 -0300

    ajustando construtor da classe Node

--- graph.h
+++ graph.h
@@ -11,7 +11,7 @@ class Node
         Node(int value, int degree) {
             this->value = value;
             this->degree = degree;
-            next = NULL;
+            nextInGraph = nextInEdges = NULL;
         }
 
         int getValue() {
