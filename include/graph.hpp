993f73a1f1f3ba1170109ab819a533d1e4d5dfb4
Ricardo Marliere <rbmarliere@gmail.com>
2015-06-01 00:22:09 -0300

    ajustando comentario

--- include/graph.hpp
+++ include/graph.hpp
@@ -130,7 +130,7 @@ class Graph
 		int getNumNodes();
 		void setNumEdges(int num);
 		int getNumEdges();
-		int Node* Graph::getHighestDegreeNode(); /* retorna o nó do maior grau do grafo */
+		int Node* Graph::getHighestDegreeNode(); /* retorna o nó com maior grau do grafo */
 		double getDegreeAverage(); /* retorna a média dos valores dos graus do grafo */
 		int getNumNodesByDegree(int degree); /* retorna a quantidade de nós com o grau @param degree*/
 		Node* insertNode(int value, int degree); /* insere nó alimentando o construtor da classe Node com os @params */
